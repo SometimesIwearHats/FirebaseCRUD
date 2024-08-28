a19a6813f55727fd692b6a705f896d6388b26a44
SometimesIwearHats <devrosatiwhite@gmail.com>
2024-08-28 14:19:55 -0400

    Added razor views for CRUD operations

---
+++ FirebaseCRUD/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace FirebaseCRUD.Models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
